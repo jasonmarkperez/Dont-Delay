26aeac493ec9006c1275712246754ddcd5dc4b13
Jason Perez <jperez@pandora.com>
2019-12-12 12:56:07 -0800

    reposition the sliders, and add some text

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -19,12 +19,11 @@ DontDelayAudioProcessorEditor::DontDelayAudioProcessorEditor (DontDelayAudioProc
     // editor's size to whatever you need it to be.
     setSize (400, 300);
     
-    
     auto& params = processor.getParameters();
     
     AudioParameterFloat* dryWetParameter = (AudioParameterFloat*)params.getUnchecked(0);
     mDryWetSlider.setRange(dryWetParameter->range.start, dryWetParameter->range.end);
-    mDryWetSlider.setBounds(0, 0, 100, 100);
+    mDryWetSlider.setBounds(0, (getHeight() / 2) - 50, 100, 100);
     mDryWetSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
     mDryWetSlider.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
     mDryWetSlider.setValue(*dryWetParameter);
@@ -44,7 +43,7 @@ DontDelayAudioProcessorEditor::DontDelayAudioProcessorEditor (DontDelayAudioProc
     
     AudioParameterFloat* feedbackParameter = (AudioParameterFloat*)params.getUnchecked(1);
     mFeedbackSlider.setRange(feedbackParameter->range.start, feedbackParameter->range.end);
-    mFeedbackSlider.setBounds(105, 0, 100, 100);
+    mFeedbackSlider.setBounds((getWidth() / 2) - 50, (getHeight() / 2) - 50, 100, 100);
     mFeedbackSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
     mFeedbackSlider.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
     mFeedbackSlider.setValue(*feedbackParameter);
@@ -64,12 +63,12 @@ DontDelayAudioProcessorEditor::DontDelayAudioProcessorEditor (DontDelayAudioProc
     
     AudioParameterFloat* delayTimeParameter = (AudioParameterFloat*)params.getUnchecked(2);
     mDelayTimeSlider.setRange(delayTimeParameter->range.start, delayTimeParameter->range.end);
-    mDelayTimeSlider.setBounds(205, 0, 100, 100);
+    mDelayTimeSlider.setBounds(getWidth() - 100, (getHeight() / 2) - 50, 100, 100);
     mDelayTimeSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
     mDelayTimeSlider.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
     mDelayTimeSlider.setValue(*delayTimeParameter);
     addAndMakeVisible(mDelayTimeSlider);
-    
+
     mDelayTimeSlider.onValueChange = [this, delayTimeParameter]{
         *delayTimeParameter = mFeedbackSlider.getValue();
     };
@@ -93,6 +92,10 @@ void DontDelayAudioProcessorEditor::paint (Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+    g.setColour(Colours::white);
+    g.drawText("Feedback",(getWidth() / 2) - 25, (getHeight() / 2) + 35, 80, 40, true);
+    g.drawText("Dry/Wet", 25, (getHeight() / 2) + 35, 80, 40, true);
+    g.drawText("Delay Time", (getWidth() - 80), (getHeight() / 2) + 35, 80, 40, true);
 }
 
 void DontDelayAudioProcessorEditor::resized()
